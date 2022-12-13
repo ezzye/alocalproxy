efb19fcdfb1a7835c521ad8028805a7f153b9b8f
ezzye <ezzy.elliott@gmail.com>
2022-12-13 21:23:08 +0000

    first draft intercept script

--- password_intercept.sh
+++ password_intercept.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-#TV_USER user name as email address
-#TV_PW password
+# example mitmdump --modify-body :~q:foo:@~/xss-exploit
+# TV_USER user name as email address
+# TV_PW password
+# TV_DOMAIN domain
+DUMMY_USER="dummy@dummy.com"
+DUMMY_PW="dummy"
+
+mitmdump --modify-body :~u "{$TV_DOMAIN}":userLoginId="{$DUMMY_USER}"&password="{$DUMMY_PW}"&rememberMe=true:userLoginId="{$TV_USER}"&password="{$TV_PW}"&rememberMe=true
 
 
